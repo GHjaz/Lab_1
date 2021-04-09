fe88a7a06814ab1dfbbd77192a7a34017e4eba02
Nikita <nodinec00@gmail.com>
2021-04-09 14:25:05 +0700

    Finale version with comments

--- personkeeper.cpp
+++ personkeeper.cpp
@@ -3,19 +3,26 @@
 
 PersonKeeper *PersonKeeper:: _instance;
 
-EStack<Person>* PersonKeeper::readPerson(std::string ilink){
-   std::ifstream iFile(ilink);
+/***    Производиться чтение данных с файла iLink.
+ ***    Данные разделяются на 3 строковые переменных(Имя, Отчество, Фамилия)
+ ***    Создается объект класса Person и его мы закидываем в стэк
+ ***    Все действия происходят в цикле
+***/
+
+EStack<Person> PersonKeeper::readPerson(std::string ilink){
+   std::ifstream iFile(ilink);      //..    Читаем из Файла с именем iLink
    std::string fName,lName,fHood;
     //.. Проверяем открыт ли файл
    if(iFile.is_open()){
-       //.. Считываем весь файл в Стэк типа Персон
+       //.. Проверяем на пустоту peek() - достает символ из файла не смещая указатель строки
        if(iFile.peek()!= EOF){
-
+       //.. Считываем пока можно считать строковые данные
         while(iFile >> lName >> fName >>fHood){
             PersonKeeper::getInstance()->_users.push(Person(lName,fName,fHood));
         }
 
        }
+    /* Иначе выдаем исключения */
        else if(iFile.peek() == EOF){
            throw exc::fileIsEmpty("File is empty");
            iFile.close();
@@ -26,13 +33,21 @@ EStack<Person>* PersonKeeper::readPerson(std::string ilink){
        throw exc::fileNotOpen("File cant be open");
        iFile.close();
     }
-   iFile.close();
-   return &PersonKeeper::getInstance()->_users; //.. Возвращаем стэк
+   iFile.close();   //..    Закрываем файл
+   return PersonKeeper::getInstance()->_users; //.. Возвращаем стэк
 };
+
+/***    Производиться чтение данных со Стэка в файл oLink.
+ ***    Объект типа Person достается из Стэка
+ ***    и с помощью методов get...Name() из него достаются 3 переменных(Ф.И.О);
+ ***    Далее с помощью потокового ввода мы записываем эти переменные в нужном формате в Файл oLink;
+ ***    Все действия происходят в цикле, выходом из которого является условие, что указатель вершины стал 0;
+***/
 
 void PersonKeeper::writePerson(std::string olink) {
     std::ofstream oFile(olink,std::ios::trunc);
     Person pers;
+    //.. Проверяем открыт ли файл
     if(oFile.is_open()){
         //.. Вытаскиваем все данные из стека пока указатель стэка не будет в нуле
         while(PersonKeeper::getInstance()->_users.getTop()!= 0){
@@ -40,8 +55,8 @@ void PersonKeeper::writePerson(std::string olink) {
             oFile << pers.getLastName()<<' '<<pers.getFirstName()<<' '<<pers.getFatherHood()<<'\n';
         }
     } else if(!oFile.is_open()){
-        throw exc::fileNotOpen("File cant be open");
+        throw exc::fileNotOpen("File cant be open");    //..    Выдаем исключения
         oFile.close();  }
-    oFile.close();
+    oFile.close();      //.. Закрываем файл
 };
 
--- personkeeper.h
+++ personkeeper.h
@@ -10,7 +10,7 @@
 
 class PersonKeeper{
 private:
-    static PersonKeeper* _instance;
+    static PersonKeeper* _instance;     //.. Создаем статический указатель на Singleton
     PersonKeeper(){};   //.. Скрываем конструктор и деструктор
     ~PersonKeeper(){};
 
@@ -18,14 +18,14 @@ public:
 
     static PersonKeeper* getInstance(){ //.. Задаем метод Instance для вызова единственного обекта класса PersonKeeper
        if (_instance == 0){
-       _instance = new PersonKeeper;
+       _instance = new PersonKeeper;    //..    Создаем объект Singleton PersonKeeper, если он не был объявлен ранее
        }
         return _instance;
-    };//.. Создаем статический указатель на Singleton
+    };
     EStack<Person> _users;  //.. Стэк классов персон
-    PersonKeeper(PersonKeeper &other) = delete; //.. Удаляем конструктор копирования и присваивания
+    PersonKeeper(PersonKeeper &other) = delete; //.. Удаляем конструктор копирования и присваивания, чтобы не было соблазна инцилизировать новый объект Singleton, и удаляем возможность инцилизации этих двух методов досутпно (c11++)
     void operator=(const PersonKeeper &) = delete;
-    EStack<Person>* readPerson(std::string);
+    EStack<Person> readPerson(std::string);
     //.. Читает из файла, пишет в стэк
     //static PersonKeeper* readPerson(std::ostream& stream);
     //.. writePerson()
